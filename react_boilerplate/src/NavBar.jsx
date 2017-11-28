9a880f62119925fdbfc7acea577e80987c618f85
Kang Wang <kwang.inbox@gmail.com>
2017-11-28 13:26:16 -0800

    NavBar

---
+++ react_boilerplate/src/NavBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <navbar className='navbar'>
+        <a href='/' className='navbar-brand'>Chatty</a>
+      </navbar>
+    )
+  }
+}
+
+export default NavBar;
