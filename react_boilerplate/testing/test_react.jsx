0e7391ded42107e13c34544ae2542cd57d939673
Kang Wang <kwang.inbox@gmail.com>
2017-11-27 23:09:33 -0800

    Boilorplate react code for chatty app; testing folder/files made for prototyping/learning.

---
+++ react_boilerplate/testing/test_react.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+import ReactDOM from "react";
+
+function Button() {
+  return (
+    <button>Go</button>
+  );
+}
+
+ReactDOM.render(<Button />, testContainer);
