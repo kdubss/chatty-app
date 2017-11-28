68808f280c8a239f51be29e28a5cec9624b3a3ad
Kang Wang <kwang.inbox@gmail.com>
2017-11-28 12:00:52 -0800

    JSX ChatBar/Message/MessageList/NavBar files.

---
+++ react_boilerplate/src/Message.jsx
@@ -0,0 +1,14 @@
+import React { Component } from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <div class="message">
+        <span class="message-username">Anonymous1</span>
+        <span class="message-content">I won't be impressed with technology until I can download food.</span>
+      </div>
+    );
+  }
+}
+
+export default Message;
---
+++ react_boilerplate/src/MessageList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class MessageList extends Component {
+  render() {
+    return (
+      <div class="message system">
+        Anonymous1 changed their name to nomnom.
+      </div>
+    )
+  }
+}
