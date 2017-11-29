a6322da3a88ef19371d8943b9a1c51be9f0ae834
Kang Wang <kwang.inbox@gmail.com>
2017-11-29 10:20:17 -0800

    Bounded 'this'; new event functions.

--- react_boilerplate/src/ChatBar.jsx
+++ react_boilerplate/src/ChatBar.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
+class ChatBar extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      content: ''
+    }
+    this.onEnterKey = this.onEnterKey.bind(this);
+    this.onNewContent = this.onNewContent.bind(this);
   }
+
+  onNewContent(event) {
+    this.setState({
+      content: event.target.value
+    });
+  }
+
+  onEnterKey(event) {
+    if (event.key === 'Enter') {
+      this.props.newChatMessage(this.state.content);
+    }
+  }
+
   render() {
     const user = this.props.currentUser.name;
-
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder={ user } />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" /> 
+        <input className="chatbar-message" placeholder="Type a message and hit ENTER" 
+          onChange={this.onNewContent} value={this.state.content} 
+          onKeyPress={this.onEnterKey} /> 
       </footer>
     );
   }
 }
 
-export default NavBar;
+export default ChatBar;
