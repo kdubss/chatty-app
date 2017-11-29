9f7bb3a1455dcd68ce1075874f28cf9862216609
Kang Wang <kwang.inbox@gmail.com>
2017-11-28 17:59:02 -0800

    Passed messages to MessageList.

--- react_boilerplate/src/App.jsx
+++ react_boilerplate/src/App.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
-import home from '../styles/home.scss';
 
+import home from '../styles/home.scss';
 import NavBar from './Navbar.jsx';
 import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id: '1'
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: '2'
+        }
+      ]
+    }
+  }
+
   render() {
     console.log("Rendering <App/>");
     return (
       <div>
         <NavBar />
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={ this.state.messages }/>
+        <ChatBar currentUser={ this.state.currentUser } />
       </div>
     );
   }
+
 }
 
 export default App;
