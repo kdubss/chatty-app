75ba72016534191dcaf4254b0acf28e31ca02100
Kang Wang <kwang.inbox@gmail.com>
2017-11-28 18:00:48 -0800

    Added placeholder username

--- react_boilerplate/src/ChatBar.jsx
+++ react_boilerplate/src/ChatBar.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const user = this.props.currentUser.name;
+
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder={ user } />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" /> 
       </footer>
     );
