4e59c557dff7be93da6e0d3af261f03abdb6d4ef
Kang Wang <kwang.inbox@gmail.com>
2017-11-28 18:02:18 -0800

    Looped over message list.

--- react_boilerplate/src/MessageList.jsx
+++ react_boilerplate/src/MessageList.jsx
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
+    const messages = this.props.messages;
+    //console.log(messages);
+    const messageObj = messages.map((message, index) => {
+      return <Message key={message.id} username={message.username} 
+        content={message.content} 
+      />
+    });
+
+    // Array of message objects (w/ username & content
+    const messageList = this.props.messages;
+
     return (  
       <main className="messages">
-        <Message />
+         { messageObj }
         <div className="message system"></div>
       </main>
     );
