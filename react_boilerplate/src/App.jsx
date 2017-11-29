cb6a6e9c56b390c8c41c8519d6fd1cbaf35aa797
Kang Wang <kwang.inbox@gmail.com>
2017-11-28 18:30:30 -0800

    updated.

--- react_boilerplate/src/App.jsx
+++ react_boilerplate/src/App.jsx
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
-import home from '../styles/home.scss';
 
+import home from '../styles/home.scss';
 import NavBar from './Navbar.jsx';
 import Message from './Message.jsx';
 import MessageList from './MessageList.jsx';
 import ChatBar from './ChatBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+          id: '1'
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good.",
+          id: '2'
+        }
+      ]
+    }
+  }
+
+  // in App.jsx
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
+  }
+
   render() {
     console.log("Rendering <App/>");
     return (
       <div>
         <NavBar />
-        <MessageList />
-        <ChatBar />
+        <MessageList messages={ this.state.messages }/>
+        <ChatBar currentUser={ this.state.currentUser } />
       </div>
     );
   }
+
 }
 
 export default App;
