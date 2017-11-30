6e10d31895b80670ed7b0966fb27f948821ebe3d
Kang Wang <kwang.inbox@gmail.com>
2017-11-29 18:35:35 -0800

    Methods for username change.

--- react_boilerplate/src/ChatBar.jsx
+++ react_boilerplate/src/ChatBar.jsx
@@ -4,10 +4,28 @@ class ChatBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      content: ''
+      content: '', 
+      value: '', 
+      username: 'Bob'
     }
     this.onEnterKey = this.onEnterKey.bind(this);
     this.onNewContent = this.onNewContent.bind(this);
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.handleUserNameSubmit = this.handleUserNameSubmit.bind(this);
+    this.userNameChange = this.userNameChange.bind(this);
+  }
+
+  handleUserNameChange(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleUserNameSubmit(event) {
+    let newUserName = this.state.username;
+    if (event.key === "Enter") {
+      alert("New User Name: ", newUserName);
+    }
   }
 
   onNewContent(event) {
@@ -16,9 +34,24 @@ class ChatBar extends Component {
     });
   }
 
+  userNameChange(event){
+    if(event.key==='Enter'){
+      var newMessage = {
+        type: 'Notification',
+        username: this.state.username
+      };
+      this.props.newChatMessage(newMessage);
+    }
+  }
+
   onEnterKey(event) {
     if (event.key === 'Enter') {
-      this.props.newChatMessage(this.state.content);
+      var newMessage = {
+        type: 'NewMessage',
+        username: this.state.username,
+        content: this.state.content
+      };
+      this.props.newChatMessage(newMessage);
     }
   }
 
@@ -26,7 +59,10 @@ class ChatBar extends Component {
     const user = this.props.currentUser.name;
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder={ user } />
+        <input className="chatbar-username" value={this.state.username} 
+          onChange={this.handleUserNameChange} onKeyPress={this.userNameChange} />
+
+
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" 
           onChange={this.onNewContent} value={this.state.content} 
           onKeyPress={this.onEnterKey} /> 
