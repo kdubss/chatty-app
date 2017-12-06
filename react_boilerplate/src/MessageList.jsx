1c03592e356f568e08561ae6f3146e4c89f0d60e
Kang Wang <kwang.inbox@gmail.com>
2017-12-05 18:11:50 -0800

    MessageList & Notification functions

--- react_boilerplate/src/MessageList.jsx
+++ react_boilerplate/src/MessageList.jsx
@@ -2,22 +2,27 @@ import React, { Component } from 'react';
 
 import Message from './Message.jsx';
 
-class MessageList extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    const messages = this.props.messages.map(message => {
-      return <Message key={message.id} username={message.username} content={message.content} />
-    });
+function Notification(props){
+  return <div className="message system">
+    {props.content}
+  </div>;
+}
 
-    return (  
-      <main className="messages">
-        { messages }
-        <div className="message system"></div>
-      </main>
-    );
-  }
+function MessageList(props){
+  const messages = props.messages.map(message => {
+    if(message.type === 'Notification'){
+      return <Notification key={message.id} content={message.content}/>
+    }
+    return <Message key={ message.id} username={message.username} content={message.content} type={message.type} />;
+  });
+
+  return (  
+    <main className="messages">
+      { messages }
+      <div className="message system"></div>
+    </main>
+  );
+
 }
 
 export default MessageList;
