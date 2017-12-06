98928bad2618059ff48b6fa4f7ad4b0fccec28c5
Kang Wang <kwang.inbox@gmail.com>
2017-12-05 18:11:00 -0800

    Conditional for if incoming message is a notification

--- react_boilerplate/src/Message.jsx
+++ react_boilerplate/src/Message.jsx
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 
 class Message extends Component {
   render() {
+    var styleNotification = {
+      textAlign: "left",
+      backgroundcolor: 'red',
+    
+    };
+    var t = this.props.type;
+    var message;
+    if(t=='NewMessage'){
+      message = (
+        <div className="styleNotification">
+          <span className="message-username">{this.props.username}</span>
+          <span className="message-content">{this.props.content}</span>
+        </div>
+      )
+    } else {
+      message = (
+        <div className="message">
+          <span className="message-username">{this.props.username}</span>
+          <span className="message-content">{this.props.content}</span>
+        </div>
+      )
+    }
     return (
-      <div className="message">
-        <span className="message-username">{this.props.username}</span>
-        <span className="message-content">{this.props.content}</span>
+
+      <div>
+        {message}
       </div>
+      // <div className="message">
+      //   <span className="message-username">{this.props.username}</span>
+      //   <span className="message-content">{this.props.content}</span>
+      // </div>
     );
   }
 }
